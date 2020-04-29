6dbe1932ae018574d3db85622bbefbb9020254cf
Sergey <sergensen@gmail.com>
2020-04-29 22:11:41 +0300

    GH-6 Add Bootstrap

--- web-control/src/main/java/com/sport/predictions/web/control/controller/CommonController.java
+++ web-control/src/main/java/com/sport/predictions/web/control/controller/CommonController.java
@@ -15,6 +15,6 @@ public class CommonController {
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
                            Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "index";
     }
 }
